944d061896d7050db6b07d4ffc6424d7c5707e2f
Dron4000 <jericho93@yandex.ru>
2021-10-30 17:18:11 +0300

    third attempt

--- spring-mvc-master/src/main/java/web/controller/CarController.java
+++ spring-mvc-master/src/main/java/web/controller/CarController.java
@@ -5,15 +5,17 @@ import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+
 import web.models.Car;
 import web.service.CarService;
 
-
-import javax.servlet.http.HttpServlet;
-import java.util.ArrayList;
 import java.util.List;
+
+import static web.dao.CarDaoImp.*;
 
 
 @Controller
@@ -24,22 +26,11 @@ public class CarController {
     private CarService carService;
 
 
-
     @GetMapping
     public String hello(@RequestParam(value = "count", defaultValue = "5", required = false) int cont, Model model) {
-        List<Car> list = new ArrayList<>();
-        Car car1 = new Car("LADA", "BLACK", 12);
-        Car car2 = new Car("BMW", "WHITE", 5);
-        Car car3 = new Car("NIVA", "GREEN", 1);
-        Car car4 = new Car("VOLVO", "BLACK", 4);
-        Car car5 = new Car("NISSAN", "GRAY", 2);
-        list.add(car1);
-        list.add(car2);
-        list.add(car3);
-        list.add(car4);
-        list.add(car5);
-        list = carService.getListCar(list, cont);
-        model.addAttribute("list", list);
+
+        List<Car> list2 = carService.getListCar(list, cont);
+        model.addAttribute("list", list2);
 
         return "cars";
     }
--- spring-mvc-master/src/main/java/web/dao/CarDaoImp.java
+++ spring-mvc-master/src/main/java/web/dao/CarDaoImp.java
@@ -10,10 +10,33 @@ import java.util.stream.Collectors;
 @Component
 public class CarDaoImp implements CarDao {
 
+  public  static   List<Car> list = new ArrayList<>();
+
+
+    static {
+
+        Car car1 = new Car("LADA", "BLACK", 12);
+        Car car2 = new Car("BMW", "WHITE", 5);
+        Car car3 = new Car("NIVA", "GREEN", 1);
+        Car car4 = new Car("VOLVO", "BLACK", 4);
+        Car car5 = new Car("NISSAN", "GRAY", 2);
+        list.add(car1);
+        list.add(car2);
+        list.add(car3);
+        list.add(car4);
+        list.add(car5);
+    }
+
+    public CarDaoImp() {
+    }
+
+
     @Override
     public List<Car> getListCar(List<Car> list, int count) {
-        if (count>=5||count==0)return list;
+        if (count >= 5 || count == 0) return list;
         return list.stream().limit(count).collect(Collectors.toList());
+
+
     }
 
 }
