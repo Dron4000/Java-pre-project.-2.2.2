93a93f61751e373e850bcf7a867b143051a86a6a
Dron4000 <90280844+Dron4000@users.noreply.github.com>
2021-12-26 20:52:15 +0300

    Update CarDaoImp.java

--- spring-mvc-master/src/main/java/web/dao/CarDaoImp.java
+++ spring-mvc-master/src/main/java/web/dao/CarDaoImp.java
@@ -33,7 +33,7 @@ public class CarDaoImp implements CarDao {
 
     @Override
     public List<Car> getListCar(List<Car> list, int count) {
-        if (count >= 5 || count == 0) return list;
+        if (count >= 5 || count <= 0) return list;
         return list.stream().limit(count).collect(Collectors.toList());
 
 
